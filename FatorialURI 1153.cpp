5070dcd5a9a41dfb8153a6dc13ca4061192c70e6
felpsman1 <85143206+felpsman1@users.noreply.github.com>
2021-08-18 14:59:32 -0300

    Add files via upload

---
+++ FatorialURI 1153.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int op;
+	int fat = 1;
+	cin >> op;
+	for(int i = 0; i < (op - 1); i++)
+	{
+		fat *= (op - i);
+	}
+	cout << fat << endl;
+	return 0;
+}
